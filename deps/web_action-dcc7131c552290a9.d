0c698c0af0d1ac03596b3f5dbd01690cdf21d69f
katteXu <417560450@qq.com>
2024-07-16 14:42:53 +0000

    Deploying to gh-pages from @ katteXu/web-2024@281ceea171e1dc1fed58920f3246124232da35a5 🚀

---
+++ deps/web_action-dcc7131c552290a9.d
@@ -0,0 +1,5 @@
+/home/runner/work/web-2024/web-2024/target/release/deps/web_action-dcc7131c552290a9: src/main.rs
+
+/home/runner/work/web-2024/web-2024/target/release/deps/web_action-dcc7131c552290a9.d: src/main.rs
+
+src/main.rs:
---
+++ web-action.d
@@ -0,0 +1,1 @@
+/home/runner/work/web-2024/web-2024/target/release/web-action: /home/runner/work/web-2024/web-2024/src/main.rs
